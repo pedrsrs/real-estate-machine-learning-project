1dba99bd8476fab24fd72d0a311d3640d37e6d34
pedrsrs <pedrosrs.dev@gmail.com>
2024-01-16 23:13:51 -0300

    Add date parsing logic

--- scraper/data_parser.py
+++ scraper/data_parser.py
@@ -1,7 +1,8 @@
+import datetime
 import re
 from kafka import KafkaConsumer
 from protobuf.scraped_data_pb2 import ScrapedDataInfo  
-from protobuf.parsed_data_pb2 import ParsedPropertyData  
+from protobuf.parsed_property_data_pb2 import ParsedPropertyData  
 
 def parse_property_type(url):
     tipo = None
@@ -12,22 +13,20 @@ def parse_property_type(url):
     return tipo
 
 def parse_property_subtype(url):
-    subtipo = None
-    if 'rts=300' in url:
-        subtipo = "apartamento"
-    elif 'rts=301' in url:
-        subtipo = "casa"
-    elif 'rts=302' in url:
-        subtipo = "casa"
-    elif 'rts=303' in url:
-        subtipo = "casa"
-    elif 'rts=304' in url:
-        subtipo = "casa"
-    elif 'rts=305' in url:
-        subtipo = "casa"
-    elif 'rts=306' in url:
-        subtipo = "casa"
-    return subtipo
+    subtype_mapping = {
+        'rts=300': 'casa de condomínio',
+        'rts=301': 'casa de vila',
+        'rts=302': 'cobertura',
+        'rts=303': 'duplex ou triplex',
+        'rts=304': 'kitnet',
+        'rts=305': 'loft'
+    }
+
+    for filter, subtype in subtype_mapping.items():
+        if filter in url:
+            return subtype
+
+    return None
 
 def parse_category(url):
     categoria = None
@@ -38,6 +37,44 @@ def parse_category(url):
         categoria = "aluguel"
     
     return categoria
+
+def parse_date(date):
+    today = datetime.date.today() 
+    month_names = {
+        "jan.": "01",
+        "fev.": "02",
+        "mar.": "03",
+        "abr.": "04",
+        "mai.": "05",
+        "jun.": "06",
+        "jul.": "07",
+        "ago.": "08",
+        "set.": "09",
+        "out.": "10",
+        "nov.": "11",
+        "dez.": "12"
+    }
+
+    transformed_date = None
+
+    if "hoje" in date.lower():
+        transformed_date = today
+    elif "ontem" in date.lower():
+        transformed_date = today - datetime.timedelta(days=1)
+    elif "anteontem" in date.lower():
+        transformed_date = today - datetime.timedelta(days=2)
+    else:
+        date_str = date.replace("de", "")
+        day, month_str = date_str.split()
+        month = month_names[month_str]
+        year = today.year if (today.month > int(month)) or (today.month == int(month) and today.day > (int(day) + 2)) else today.year - 1
+        transformed_date = datetime.date(year, int(month), int(day))
+
+    if transformed_date is not None:
+        transformed_date_str = transformed_date.strftime('%d/%m/%Y')
+    
+    return transformed_date_str
+
 
 def parse_other_costs(other_costs):
     iptu = condominio = None
@@ -61,6 +98,16 @@ def parse_location(location):
 
     return cidade, bairro
 
+def parse_region(url):
+    pattern = re.compile(r'/([^?/]+)\?')
+    match = pattern.search(url)
+
+    if match:
+        result = match.group(1).replace('-', ' ')
+        return result
+    else:
+        return None
+
 def parse_information(labels):
     quartos = area = vagas_garagem = banheiros = None
 
@@ -76,20 +123,13 @@ def parse_information(labels):
 
     return quartos, area, vagas_garagem, banheiros
 
-def send_db(labels):
+def send_postgres_venda(message):
     quartos = area = vagas_garagem = banheiros = None
 
-    for item in labels:
-        if 'quarto' in item:
-            quartos = int(item.split()[0])
-        elif 'metro' in item:
-            area = int(item.split()[0])
-        elif 'garagem' in item:
-            vagas_garagem = int(item.split()[0])
-        elif 'banheiro' in item:
-            banheiros = int(item.split()[0])
 
-    return quartos, area, vagas_garagem, banheiros
+def send_postgres_aluguel(message):
+    quartos = area = vagas_garagem = banheiros = None
+
 
 def main():
     conf = {
@@ -115,12 +155,12 @@ def main():
                 iptu, condominio = parse_other_costs(scraped_data_info.other_costs)
                 quartos, area, vagas_garagem, banheiros = parse_information(scraped_data_info.labels)
                 cidade, bairro = parse_location(scraped_data_info.location)
+                regiao = parse_region(scraped_data_info.url)
 
                 message = ParsedPropertyData(
                     titulo=titulo,
                     tipo=tipo,
                     subtipo=subtipo,
-                    categoria=categoria,
                     valor=valor,
                     iptu=iptu,
                     condominio=condominio,
@@ -129,8 +169,14 @@ def main():
                     vagas_garagem=vagas_garagem,
                     banheiros=banheiros,
                     cidade=cidade,
-                    bairro=bairro
+                    bairro=bairro,
+                    regiao=regiao
                 )
+
+                if categoria == "venda":
+                    send_postgres_venda(message)
+                elif categoria == "aluguel":
+                    send_postgres_aluguel(message)
 
     except KeyboardInterrupt:
         pass
@@ -140,5 +186,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    
