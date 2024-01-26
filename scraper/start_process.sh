c6606a69c028b4f3e4f27a6fe3ac824ab1252696
pedrsrs <pedrosrs.dev@gmail.com>
2024-01-25 23:05:30 -0300

    Add start_process.sh

---
+++ scraper/start_process.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CSV_FILE="olx_links.csv"
+
+check_csv_status() {
+    while IFS=, read -r url qtd status; do
+        if [ "$status" != "finished" ]; then
+            return 0  
+        fi
+    done < "$CSV_FILE"
+    return 1  
+}
+
+start_time=$(date +%s)
+
+echo -n > olx_links.csv
+echo -n > unfiltered_links.txt
+
+python3 regions.py
+python3 link_scraper.py
+
+while true; do
+    if check_csv_status; then
+        python3 property_scraper.py
+    else
+        echo "All rows have status 'finished'. No action required."
+    fi
+    sleep 5
+done
+
+end_time=$(date +%s)
+elapsed_time_minutes=$(echo "scale=2; ($end_time - $start_time) / 60" | bc)
+
+echo "Elapsed time: $elapsed_time minutes"
