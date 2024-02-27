09c7f47f6b6619af6faff297fea2194fe2b0a1f1
pedrsrs <pedrosrs.dev@gmail.com>
2024-02-26 23:47:50 -0300

    Update sidebar

---
+++ dashboard/components/exportacao.py
@@ -0,0 +1,10 @@
+import plotly.express as px
+from dash import html, dcc
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+import numpy as np
+import pandas as pd
+import psycopg2
+
+layout = dbc.Col([
+                ])
--- dashboard/components/sidebar.py
+++ dashboard/components/sidebar.py
@@ -1,17 +1,42 @@
 import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
+import dash_daq as daq
 
 def get_sidebar_layout():
     sidebar = dbc.Col([
         html.Div(className="sidebar", children=[
-            dbc.Button(id="estado",
-                    children=[html.H1("Venda", className="botao")
-                        ], style={'background-color': '#07A182'}),
-            html.H1("Geral", className="text-primary"),
-            html.H1("Bairros", className="text-primary"),
-            html.H1("Previsões", className="text-primary"),
-            html.H1("Busca e Exportação", className="text-primary"),
+            html.Div(className='stateButton', id="estado",
+                    children=[html.H1("Venda", className="botao"),
+                        ]),
+                    
+            html.Div(className="sidebar-pages-container", children=[
+                        html.A(href="google.com", className="sections-button", children=[
+                            html.I(className="fa-solid fa-globe"),
+                            html.A("Geral", className="text-primary")
+                        ]),
+                        html.Div(className='line'),
+                        html.A(href="google.com", className="sections-button", children=[
+                            html.I(className="fa-solid fa-map-location-dot"),
+                            html.A("Bairros", className="text-primary")
+                        ]),
+                        html.Div(className='line'),
+                        html.A(href="google.com", className="sections-button", children=[
+                            html.I(className="fa-solid fa-clock-rotate-left"),
+                            html.A("Histórico", className="text-primary")
+                        ]),
+                        html.Div(className='line'),
+                        html.A(href="google.com", className="sections-button", children=[
+                            html.I(className="fa-solid fa-chart-line"),
+                            html.A("Previsões", className="text-primary")
+                        ]),
+                        html.Div(className='line'),
+                        html.A(href="google.com", className="sections-button", children=[
+                            html.I(className="fa-regular fa-paste"),
+                            html.A("Busca e Exportação", className="text-primary")
+                        ]),
+                        
+                    ])
         ])
     ])
     return sidebar
